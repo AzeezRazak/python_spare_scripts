a2a5d9f9b664a352d612d3019805a4743111f580
Abdul Azeez <azeezshahrizal@gmail.com>
2018-09-17 13:35:57 +0800

    Update input_string.py

--- string/input_string.py
+++ string/input_string.py
@@ -2,7 +2,18 @@ ii = int(input())
 dd = float(input())
 ss = input() #Input String
 
+n = int(input())
+arr = map(int, input().split())
+print(sorted(list(set(arr)))[-2])
 
-    n = int(input())
-    arr = map(int, input().split())
-    print(sorted(list(set(arr)))[-2])
+
+# Input
+# This is a string
+# Output
+# This-is-a-string
+
+def split_and_join(line):
+    
+    a = line.split(' ')
+    a = '-'.join(a)
+    return(a)
